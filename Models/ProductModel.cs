dbed96c95781a069ed8f7a93f12ddca0d46232f2
Anirudhdha Chothani <23030501015@darshan.ac.in>
2024-11-18 08:40:15 +0530

    model commit

---
+++ Models/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Forjob.Models
+{
+    public class ProductModel
+    {
+        [Required]
+        public int? ProductID { get; set; }
+        [Required]
+        public String ProductName { get; set; }
+        [Required]
+        public double ProductPrice { get; set; }
+        [Required]
+        public string ProductCode { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int UserID { get; set; } = 101;
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Forjob.Models
+{
+    public class UserModel
+    {
+       
+            public int? UserID { get; set; }
+
+            [Required(ErrorMessage = "User Name is Required")]
+            public string UserName { get; set; }
+
+            [Required]
+            [Display(Name = "Email")]
+            [EmailAddress(ErrorMessage = "Require")]
+            [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Your Email is not valid.")]
+            public string Email { get; set; }
+
+            [Required]
+            [Display(Name = "Password")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile Number Must Be 10 Digits!")]
+            public string MobileNo { get; set; }
+
+            [Required(ErrorMessage = "Address is Required")]
+            public string Address { get; set; }
+
+            [Required]
+            [Display(Name = "Active")]
+            public bool IsActive { get; set; }
+        
+    }
+}
